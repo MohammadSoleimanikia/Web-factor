90ebaaaf4818408bf659b59efd43733b991826a0
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-01-03 10:45:18 +0330

    update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,32 +1,44 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
         languageOptions: {
-            globals: globals.browser,
             parser: "@typescript-eslint/parser",
+            globals: globals.browser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+            ecmaFeatures: {
+                jsx: true,
+            },
         },
         plugins: {
             react,
             "@typescript-eslint": tseslint,
+            "simple-import-sort": simpleImportSort,
         },
+        extends: [
+            "eslint:recommended",
+            "plugin:react/recommended",
+            "plugin:@typescript-eslint/recommended",
+        ],
         rules: {
-            "react/react-in-jsx-scope": "off",
-            "@typescript-eslint/no-explicit-any": "off", // allow any
+            "react/react-in-jsx-scope": "off", // React 17+ JSX doesn’t need import React
+            "@typescript-eslint/no-explicit-any": "off", // allow any type
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+            // optional: disable other conflicting sorting rules
+            "sort-imports": "off",
+            "import/order": "off",
         },
         settings: {
             react: {
                 version: "detect",
             },
         },
-        extends: [
-            "plugin:@typescript-eslint/recommended",
-            "eslint:recommended",
-            "plugin:react/recommended",
-        ],
     },
 ]);
