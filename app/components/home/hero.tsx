4d196e24fa5930dcd91b4333bc0b03f4a40b6f0d
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-04-24 10:10:02 +0330

    update Home Page Design

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,54 +1,38 @@
-import { motion } from "motion/react";
-import { NavLink } from "react-router";
-
-import { Button } from "../ui/button";
 export default function Hero() {
-    const scrollToAbout = () => {
-        const aboutSection = document.getElementById("about");
-        aboutSection?.scrollIntoView({ behavior: "smooth" });
-    };
     return (
-        <motion.div
-            initial={{
-                opacity: 0,
-                y: 40,
-            }}
-            animate={{
-                opacity: 1,
-                y: 0,
-            }}
-            transition={{
-                duration: 0.6,
-            }}
-            className="container mx-auto mt-10 md:mt-0 min-h-[calc(100vh-80px)] flex flex-col sm:flex-row  sm:justify-between  items-center"
-        >
-            <div className="w-full sm:w-3/6  flex flex-col gap-3 space-y-0 md:space-y-8">
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
-                    سریع، دقیق، آماده چاپ
-                </h1>
-                
-                <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl ">
-                    فاکتور حرفه‌ای بساز و همون لحظه برای مشتری بفرست.
-                </p>
-                <p className="text-sm sm:text-md md:text-lg lg:text-xl font-light text-gray-600 dark:text-gray-400">
-                    یه فاکتور‌ساز سریع و مخصوص فروشگاه‌های آنلاین؛ محصولاتو
-                    اضافه کن، فاکتور بساز، آنلاین برای مشتری بفرست و با QR Code
-                    همیشه قابل دسترس نگهش دار. همه‌چی ساده، تمیز و بدون دردسر.
-                </p>
-                <div className="flex gap-3 mt-2">
-                    <NavLink to={"/dashboard"}>
-                        <Button>شروع کنید</Button>
-                    </NavLink>
-                    <Button variant={"outline"} onClick={scrollToAbout}>
-                        درباره ما
-                    </Button>
+        <section className="lg:pt-20 pt-0 container mx-auto h-full">
+            <div className="rounded-2xl bg-[#E9F5F3] py-10 overflow-hidden  2xl:py-16 xl:py-8  lg:rounded-tl-2xl lg:rounded-bl-2xl ">
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div className="grid grid-cols-1 gap-14 items-center lg:grid-cols-12 lg:gap32">
+                        <div className="w-full xl:col-span-5 lg:col-span-6 2xl:-mx-5 xl:mx-0">
+                            
+                            <h1 className="py-8 text-center text-gray-900 font-bold font-manrope text-5xl lg:text-right leading-[70px]">
+                                
+                                <span className="text-primary">
+                                    فاکتور حرفه‌ای {" "}
+                                </span>
+                                بساز و همون لحظه برای مشتری بفرست
+                            </h1>
+                            <p className=" text-gray-500 text-lg text-center lg:text-right">
+                                همه چیز برای ساخت، ارسال و مدیریت فاکتور در چند ثانیه آماده است.
+                            </p>
+                            
+                                <button className="bg-primary rounded-full py-3 px-7 text-base font-semibold text-white cursor-pointer transition-all duration-500 lg:w-fit w-full">
+                                    شروع کنید
+                                </button>
+                        </div>
+                        <div className="w-full xl:col-span-7  lg:col-span-6 block">
+                            <div className="w-full  sm:w-auto lg:w-[60.8125rem] xl:ml-16">
+                                <img
+                                    src="/hero/light.png"
+                                    alt="Dashboard image"
+                                    className="rounded-l-3xl object-cover w-full lg:h-auto"
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <img
-                className="sm:w-3/6  w-full sm:block mt-5"
-                src="/hero-section.svg"
-                alt=""
-            />
-        </motion.div>
+        </section>
     );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import About from "@/components/home/about";
 import Footer from "@/components/home/footer";
 import Header from "@/components/home/header";
-import Hero from "~/components/home/hero";
+import Hero from "@/components/home/hero";
+
 
 
 export function meta() {
@@ -13,7 +14,7 @@ export function meta() {
 
 export default function Home() {
     return (
-        <main className="mx-auto px-4 sm:px-10 md:px-20 lg:px-32">
+        <main className="mx-auto ">
             <Header />
             <Hero />
             <About />
