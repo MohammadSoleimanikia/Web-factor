b51596b89138a0d309e8b0fa00bcbf691686bd3c
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-13 09:58:07 +0330

    feat(routes): add payment result and subscription management routes

--- app/routes.ts
+++ app/routes.ts
@@ -9,33 +9,29 @@ export default [
     index("routes/home.tsx"),
     route("public/:invoiceToken", "routes/invoices/publicInvoice.tsx"),
     route("*", "routes/NotFound.tsx"), // ✅ 404
+    
+    // ✅ روت‌های پرداخت
+    route("payment/result", "routes/payment/result.tsx"),
+    
     layout("routes/guestOnly.tsx", [
         route("login", "routes/logIn.tsx"),
         route("demo", "routes/demoInvoice.tsx"),
         route("demo-invoice/preview", "routes/demoInvoicePreview.tsx"),
     ]),
+    
     // layout
     layout("routes/dashboard/protected.tsx", [
         layout("routes/dashboard/dashboardLayout.tsx", [
+            route("subscription", "routes/dashboard/subscription.tsx"),
             route("dashboard", "routes/dashboard/dashboard.tsx"),
             route("products", "routes/products.tsx"),
-
             route("invoices", "routes/invoices/invoices.tsx"),
-
             route("invoices/new", "routes/invoices/newInvoice.tsx"),
-
             route("invoices/edit/:id", "routes/invoices/editInvoice.tsx"),
-
             route("invoices/:id", "routes/invoices/invoice.tsx"),
-
             route("profile", "routes/profile.tsx"),
-
             route("customers", "routes/customers.tsx"),
-
-            route(
-                "customers/:id/reports",
-                "routes/customers/customerReports.tsx",
-            ),
+            route("customers/:id/reports", "routes/customers/customerReports.tsx"),
         ]),
     ]),
 ] satisfies RouteConfig;
