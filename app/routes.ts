d005940fe38b1a7c425f1af5823b5fe6e252ea8d
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-25 12:05:56 +0330

    add purchase route

--- app/routes.ts
+++ app/routes.ts
@@ -7,32 +7,49 @@ import {
 
 export default [
     index("routes/home.tsx"),
+
     route("public/:invoiceToken", "routes/invoices/publicInvoice.tsx"),
-    route("*", "routes/NotFound.tsx"), // ✅ 404
-    
-    // ✅ روت‌های پرداخت
+
     route("payment/result", "routes/payment/result.tsx"),
-    
+
     layout("routes/guestOnly.tsx", [
         route("login", "routes/logIn.tsx"),
         route("demo", "routes/demoInvoice.tsx"),
         route("demo-invoice/preview", "routes/demoInvoicePreview.tsx"),
     ]),
-    
-    // layout
+
     layout("routes/dashboard/protected.tsx", [
         layout("routes/dashboard/dashboardLayout.tsx", [
             route("subscription", "routes/dashboard/subscription.tsx"),
             route("dashboard", "routes/dashboard/dashboard.tsx"),
+
             route("products", "routes/products.tsx"),
+
             route("invoices", "routes/invoices/invoices.tsx"),
             route("invoices/new", "routes/invoices/newInvoice.tsx"),
             route("invoices/edit/:id", "routes/invoices/editInvoice.tsx"),
             route("invoices/:id", "routes/invoices/invoice.tsx"),
+
             route("profile", "routes/profile.tsx"),
+
             route("customers", "routes/customers.tsx"),
-            route("customers/:id/reports", "routes/customers/customerReports.tsx"),
+            route(
+                "customers/:id/reports",
+                "routes/customers/customerReports.tsx",
+            ),
+
             route("categories", "routes/categories.tsx"),
+
+            // Purchase Invoice routes
+            route("purchase-invoices", "routes/purchaseInvoices.tsx"),
+            route("purchase-invoices/new", "routes/purchaseInvoicesNew.tsx"),
+            route(
+                "purchase-invoices/edit/:id",
+                "routes/purchaseInvoicesEdit.tsx",
+            ),
+            route("purchase-invoices/:id", "routes/purchaseInvoicesId.tsx"),
         ]),
     ]),
+
+    route("*", "routes/NotFound.tsx"),
 ] satisfies RouteConfig;
